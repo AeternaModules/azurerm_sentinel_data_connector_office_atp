371e47dca843431e1cbb1c7739d2633910941a17
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:15 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "sentinel_data_connector_office_atps" {
-  description = "All sentinel_data_connector_office_atp resources"
-  value       = azurerm_sentinel_data_connector_office_atp.sentinel_data_connector_office_atps
-}
 output "sentinel_data_connector_office_atps_log_analytics_workspace_id" {
-  description = "List of log_analytics_workspace_id values across all sentinel_data_connector_office_atps"
-  value       = [for k, v in azurerm_sentinel_data_connector_office_atp.sentinel_data_connector_office_atps : v.log_analytics_workspace_id]
+  description = "Map of log_analytics_workspace_id values across all sentinel_data_connector_office_atps, keyed the same as var.sentinel_data_connector_office_atps"
+  value       = { for k, v in azurerm_sentinel_data_connector_office_atp.sentinel_data_connector_office_atps : k => v.log_analytics_workspace_id }
 }
 output "sentinel_data_connector_office_atps_name" {
-  description = "List of name values across all sentinel_data_connector_office_atps"
-  value       = [for k, v in azurerm_sentinel_data_connector_office_atp.sentinel_data_connector_office_atps : v.name]
+  description = "Map of name values across all sentinel_data_connector_office_atps, keyed the same as var.sentinel_data_connector_office_atps"
+  value       = { for k, v in azurerm_sentinel_data_connector_office_atp.sentinel_data_connector_office_atps : k => v.name }
 }
 output "sentinel_data_connector_office_atps_tenant_id" {
-  description = "List of tenant_id values across all sentinel_data_connector_office_atps"
-  value       = [for k, v in azurerm_sentinel_data_connector_office_atp.sentinel_data_connector_office_atps : v.tenant_id]
+  description = "Map of tenant_id values across all sentinel_data_connector_office_atps, keyed the same as var.sentinel_data_connector_office_atps"
+  value       = { for k, v in azurerm_sentinel_data_connector_office_atp.sentinel_data_connector_office_atps : k => v.tenant_id }
 }
 
